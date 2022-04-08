19a2b0de95a98618a5f593579db4e084f59328fb
Bria White <bria.awhite@gmail.com>
2022-04-08 15:43:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+const employee = {
+    name: "Bria",
+    streetAddress: "221 Bakers St",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updatedEmployee = { ...employee };
+
+    updatedEmployee[key] = value;
+
+    return updatedEmployee;
+}
+
+// const newEmployee = updateEmployeeWithKeyAndValue(
+//     employee,
+//     "name",
+//     "sherlock"
+// );
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const copyOfEmployee = {...employee}
+    delete copyOfEmployee[key]
+
+    return copyOfEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+
+    return employee
+}
+
+
